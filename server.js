a1d7ea7e8040d4bd5f32169169ae15d4951b7b62
Tay J Solis <tayjsolis@gmail.com>
2018-10-09 12:16:22 -0700

    Pseudocode

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/** Con figuration **/
 const express = require('express');
 const app = express();
 const db = require('./models');
@@ -19,9 +20,22 @@ app.use(function(req, res, next) {
 
 app.use(express.static('public'));
 
+/* Routes */
+
+//Root Route
 app.get('/', (req,res) =>{
   res.sendFile('/views/index.html', {root: __dirname});
 });
+
+//Read: Get all insects in database
+
+//Read: Gets individual insect entry by id
+
+//Create: Creates new insect entry
+
+//Delete: Destroys existing insect entry
+
+//Update: Edits existing insect entry
 
 app.listen(port, () => {
   console.log(`Bug app is listening on port:${port}`);
